24a732831e47a49f874d76a590ad860632a7fe2b
dev_Mart <martumbaocommude@gmail.com>
2024-02-27 23:04:41 +0800

    add route and import GuestsController

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GuestsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/guests', function() {
-    return 'This is Guest API';
-});
+Route::get('/guests', [GuestsController::class, 'index']);
 
 
-// Route::get('/guests', 'guests@index');
 // Route::get('/products/{id}', 'ProductController@show');
 // Route::post('/products', 'ProductController@store');
 // Route::put('/products/{id}', 'ProductController@update');
